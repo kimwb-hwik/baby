3971c90658314872750bf1054c04e6d39aa416c8
dnqhdqhd <dnqhdqhd@gmail.net>
2022-02-22 21:45:06 +0900

    메인,마이페이지

--- pop/js/front.js
+++ pop/js/front.js
@@ -7,6 +7,7 @@ $(function(){
 	}
 	Headerfixed();
 	qnalist();
+	
 		
 
 
@@ -96,9 +97,55 @@ function Sidepopup(){
 	
 }
 
-//end
-	
-});
+
+
+}); //end
+
+
+
+function openNav() {
+	document.getElementById("mySidepanel").style.width = "100%";
+  }
+  
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+document.getElementById("mySidepanel").style.width = "0";
+}
+
+
+  //include
+  function includeHTML() {
+	var z, i, elmnt, file, xhttp;
+	/* Loop through a collection of all HTML elements: */
+	z = document.getElementsByTagName("*");
+	for (i = 0; i < z.length; i++) {
+	  elmnt = z[i];
+	  /*search for elements with a certain atrribute:*/
+	  file = elmnt.getAttribute("w3-include-html");
+	  if (file) {
+		/* Make an HTTP request using the attribute value as the file name: */
+		xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+		  if (this.readyState == 4) {
+			if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+			if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+			/* Remove the attribute, and call this function once more: */
+			elmnt.removeAttribute("w3-include-html");
+			includeHTML();
+		  }
+		}
+		xhttp.open("GET", file, true);
+		xhttp.send();
+		/* Exit the function: */
+		return;
+	  }
+	}
+  }
+
+
+
+
+
 
 
 
